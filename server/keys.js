b5b075f4697fadd51c2e3f554d0dc9a208b7b642
Darrell Snow <darrell.snow@iengagelabs.com>
2019-11-28 14:32:33 -0800

    Added nginx and dev Dockerfiles.

--- server/keys.js
+++ server/keys.js
@@ -2,8 +2,8 @@ module.exports = {
     redisHost: process.env.REDIS_HOST,
     redisPort: process.env.REDIS_PORT,
     pgHost: process.env.PGHOST,
-    pgUser: process.env.PGUSER,
-    pgDatabase: process.env.PGDATABASE,
-    pgPassword: process.env.PGPASSWORD,
+    pgUser: process.env.POSTGRES_USER,
+    pgDatabase: process.env.POSTGRES_DB,
+    pgPassword: process.env.POSTGRES_PASSWORD,
     pgPort: process.env.PGPORT
 };
